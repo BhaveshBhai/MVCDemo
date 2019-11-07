3c8d0e3bcaa3613a33ab106b3b58278de9b51b3b
bhavesh narola <bhavesh@abacus.co>
2019-11-08 00:03:20 +1100

    Code Added

---
+++ SafetracWebApp/Controllers/UsersController.cs
@@ -0,0 +1,57 @@
+﻿using SafetracWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SafetracWebApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        UserModelView userModel = new UserModelView();
+        // GET: Users
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public JsonResult List()
+
+        {
+            return Json(new { data = userModel.GetAllUsers() }, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult UsersList()
+        {
+            return View();
+        }
+        [HttpPost]
+        public JsonResult LoadAllUsersData()
+
+        {
+            var draw = Request.Form.GetValues("draw").FirstOrDefault();
+            var start = Request.Form.GetValues("start").FirstOrDefault();
+            var length = Request.Form.GetValues("length").FirstOrDefault();
+            var search = Request.Form.GetValues("search[value]").FirstOrDefault();
+            //Find Order Column
+            var sortColumn = Request.Form.GetValues("columns[" + Request.Form.GetValues("order[0][column]").FirstOrDefault() + "][name]").FirstOrDefault();
+            var sortColumnDir = Request.Form.GetValues("order[0][dir]").FirstOrDefault();
+            int pageSize = length != null ? Convert.ToInt32(length) : 0;
+            int PageNumber = start != null ? Convert.ToInt32(start) / Convert.ToInt32(length) + 1 : 0;
+
+            var lstUsers = userModel.LoadAllUsers(search.ToString(), PageNumber, pageSize, sortColumn, sortColumnDir);
+            int TotalRecord = lstUsers.Select(x => x.MaxRows.Value).First();
+            return Json(new { draw = draw, recordsFiltered = TotalRecord, recordsTotal = TotalRecord, data = lstUsers }, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult Add()
+        {
+            return PartialView("_AddUsers");
+        }
+        [HttpPost]
+        public ActionResult AddUsers(User users)
+        {
+            var result = userModel.AddUser(users);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SafetracWebApp/Models/UserModelView.cs
@@ -0,0 +1,84 @@
+﻿using SafetracWebApp.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SafetracWebApp.Models
+{
+    public class UserModelView
+    {
+        public int Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Email { get; set; }
+        public DateTime? CreatedDate { get; set; }
+
+        public List<GetAllUsers_Result> GetAllUsers()
+        {
+            List<GetAllUsers_Result> lstUser = new List<GetAllUsers_Result>();
+            try
+            {
+                using (SafetracEntities db = new SafetracEntities())
+                {
+                    lstUser = db.GetAllUsers()
+                        .Select(x=> new GetAllUsers_Result
+                        {first_name= x.first_name,
+                       last_name= x.last_name,
+                       date_created= x.date_created,
+                      email_address= EncryptionHelper.Decrypt(x.email_address),
+                       id= x.id }).ToList();
+                    return lstUser;
+                }
+            }
+            catch (Exception e)
+            {
+                return lstUser;
+            }
+
+        }
+
+        public List<ListUsers_Result> LoadAllUsers(string Search, int StartPage, int Lenght, string Sort, string order)
+        {
+            List<ListUsers_Result> lstUser = new List<ListUsers_Result>();
+            try
+            {
+                using (SafetracEntities db = new SafetracEntities())
+                {
+                    lstUser = db.ListUsers(Search, StartPage == 0 ? 1 : StartPage, Lenght, Sort, order).Select(x => new ListUsers_Result
+                    {
+                        first_name = x.first_name,
+                        last_name = x.last_name,
+                        date_created = x.date_created,
+                        email_address = EncryptionHelper.Decrypt(x.email_address),
+                        id = x.id
+                    }).ToList();
+                    return lstUser;
+                }
+            }
+            catch (Exception e)
+            {
+                return lstUser;
+            }
+
+        }
+        public int AddUser(User user)
+        {
+            try
+            {
+                using (SafetracEntities db = new SafetracEntities())
+                {
+                    user.email_address = EncryptionHelper.Encrypt(user.email_address);
+                    user.date_created = DateTime.UtcNow;
+                    user.date_modified = DateTime.UtcNow;
+                    db.Users.Add(user);
+                    return (int)db.SaveChanges();
+                }
+            }
+            catch (Exception e)
+            {
+                return 0;
+            }
+        }
+    }
+}
